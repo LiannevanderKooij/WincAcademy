61938194047adf562061254f7f95532c6bdb3e78
LiannevanderKooij <78352636+LiannevanderKooij@users.noreply.github.com>
2021-06-14 20:22:32 +0200

    Add files via upload

---
+++ 10_opdracht_eerste_Javascript/index.js
@@ -0,0 +1,14 @@
+console.log('Hello Winc Academy');
+        let naam = 'Lianne';
+        console.log(naam);
+        console.log(4+4);
+        console.log('4'+'4');
+        console.log(4-3);
+        console.log(5*6);
+        console.log(4/2);
+        console.log(9%2);
+
+        let age = 28;
+        console.log(typeof age);
+        age = '28';
+        console.log(typeof age); 
