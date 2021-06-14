b5ccc0137b6f046b491cd7eaf639421b53c79032
LiannevanderKooij <78352636+LiannevanderKooij@users.noreply.github.com>
2021-06-14 22:10:12 +0200

    Add files via upload

---
+++ 12_opdracht_if_else_statements/script.js
@@ -0,0 +1,25 @@
+const age = 16;
+
+if (age >= 18) {
+    console.log('je mag naar binnen');
+
+}else {
+    console.log('je bent helaas te jong, je mag niet naar binnen');
+}
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log('je mag een kaartje voor ladiesnight kopen');
+} else {
+    console.log('je mag helaas niet naar ladiesnight');
+}
+
+const driverStatus = 'bb';
+
+if (driverStatus == 'bob') {
+    console.log('je mag naar huis rijden');
+
+}else {
+    console.log('je mag niet zelf naar huis rijden');
+}
