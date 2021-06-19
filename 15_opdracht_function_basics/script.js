5e48a8d37b372aed2470bc22daac58476ad61014
LiannevanderKooij <78352636+LiannevanderKooij@users.noreply.github.com>
2021-06-19 11:45:58 +0200

    Add files via upload

---
+++ 15_opdracht_function_basics/script.js
@@ -0,0 +1,18 @@
+const makePizza = function(){
+    console.log('roll out dough');
+    console.log('put sauce and topping on dough');
+    console.log('bake pizza in the oven');
+};
+
+const makeSushi = function(){
+    console.log('cook rice');
+    console.log('put rice and salmon on nori sheet');
+    console.log('roll sushi');
+    console.log('cut sushi');
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
